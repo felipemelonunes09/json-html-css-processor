d4753bc15347500cf1fde28ece968ebcda246aa3
felipemelonunes09 <felipe.melonunes09@gmail.com>
2021-10-25 16:31:44 -0300

    dom object creation

---
+++ src/modules/factories/ElementFactory.js
@@ -0,0 +1,25 @@
+const { IdentityManager, Visual } = require("../../utils");
+
+class ElementFactory {
+
+    constructor(identityManager = IdentityManager) {
+        this.identityManager = IdentityManager();
+    }
+
+    build(element) {
+
+        if (element.tag == undefined || element.tag == null)
+            return false;
+        
+        let domElement = document.createElement(element.tag)
+        domElement.id = (element.id == null || element.id == undefined) ? this.identityManager.genKey() : element.id;
+        domElement.innerHTML = (element.inner == null || element.inner == undefined) ? '' : element.inner;
+
+        Visual(domElement).apply(element.styles)
+        
+
+        return domElement;
+    } 
+}
+
+module.exports = ElementFactory;
