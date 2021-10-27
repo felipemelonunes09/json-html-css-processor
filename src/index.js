d996196f32ad12c4b6b8da0dd70e7fb7ebd83efe
felipemelonunes09 <felipe.melonunes09@gmail.com>
2021-10-27 11:25:49 -0300

    Import of testing itens

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { essentials, p, div, section } = require('./buildin/essentials.js');
 const model = require('./modules/JHCP.js');
 const JHCP = model()
 
@@ -39,4 +40,18 @@ object = {
 const element = JHCP.build(object)
 document.querySelector('body').appendChild(element)
 
+const obj1 = { 
+    ...div('Div teste', 'div-teste', ['class-1', 'class-2'], 
+    [
+        p('Texto teste', undefined, ['p'], [
 
+        ]),
+        div('div-1', undefined, ['class-3'], [
+            div('Um texto super refinado para minha div'),
+            div('Outro texto super refinado para minha div')
+        ])
+    ])
+}
+
+const el1 = JHCP.build(obj1)
+console.log(el1)
