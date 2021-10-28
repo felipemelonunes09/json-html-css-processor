9f29234f4490f8702963f4031421e7697737518b
felipemelonunes09 <felipe.melonunes09@gmail.com>
2021-10-28 10:27:07 -0300

    Image object test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const { essentials, p, div, section } = require('./buildin/essentials.js');
+const { essentials, p, div, section, img } = require('./buildin/essentials.js');
 const model = require('./modules/JHCP.js');
 const JHCP = model()
 
 
-object = { 
+const object = { 
     tag: 'div',
     styles: ['class-1', 'class-2'],
     childs: [
@@ -37,8 +37,8 @@ object = {
     ]
 };
 
-const element = JHCP.build(object)
-document.querySelector('body').appendChild(element)
+//const element = JHCP.build(object)
+//document.querySelector('body').appendChild(element)
 
 const obj1 = { 
     ...div('Div teste', 'div-teste', ['class-1', 'class-2'], 
@@ -48,10 +48,12 @@ const obj1 = {
         ]),
         div('div-1', undefined, ['class-3'], [
             div('Um texto super refinado para minha div'),
-            div('Outro texto super refinado para minha div')
+            img('https://www.viewhotels.jp/ryogoku/wp-content/uploads/sites/9/2020/03/test-img.jpg')
         ])
     ])
 }
 
 const el1 = JHCP.build(obj1)
-console.log(el1)
+console.log(el1);
+
+document.querySelector('body').appendChild(el1)
