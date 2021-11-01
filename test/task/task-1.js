aa9d9ea27d2189d109b4a0c5dd85c35e576aff93
felipemelonunes09 <felipe.melonunes09@gmail.com>
2021-11-01 15:19:40 -0300

    Task 1 implemented

---
+++ test/task/task-1.js
@@ -0,0 +1,19 @@
+const { button } = require("../../src/buildin/essentials")
+
+const task_1 = () => {
+    const features = ['tag', 'inner', 'id']
+    const expected = {  
+        id: 'button-1'
+    }
+
+    let obj_button = button('new button', () => console.log('testing item') , 'button-1', [], [])
+    features.forEach(feat => {
+        if (expected[ feat ] != obj_button[feat]) 
+            throw new Error('Objeto com propriedade invalidas')
+    })
+
+    console.log('[TASK - 1] - SUCCESS')
+}
+
+module.exports = task_1;
+
